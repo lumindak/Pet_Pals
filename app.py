848c8f0ca812eed595c9b1e57a85bdb4fcf30a89
luminda <61852344+lumindak@users.noreply.github.com>
2020-07-11 10:47:59 -0400

    Starter files

---
+++ app.py
@@ -0,0 +1,85 @@
+# import necessary libraries
+from models import create_classes
+import os
+from flask import (
+    Flask,
+    render_template,
+    jsonify,
+    request,
+    redirect)
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+#################################################
+# Database Setup
+#################################################
+
+from flask_sqlalchemy import SQLAlchemy
+
+#if database does not exist it will switch to local sqlite database(when database env is not set up)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or "sqlite:///db.sqlite"
+
+# Remove tracking modifications
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+#create connection
+db = SQLAlchemy(app)
+
+Pet = create_classes(db)
+
+# create route that renders index.html template
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+# Query the database and send the jsonified results
+#POST method is a safer way to talk between front and back end
+#GET is similar not that safe
+@app.route("/send", methods=["GET", "POST"])
+def send():
+    if request.method == "POST":
+        name = request.form["petName"]
+        lat = request.form["petLat"]
+        lon = request.form["petLon"]
+
+        pet = Pet(name=name, lat=lat, lon=lon)
+        db.session.add(pet)
+        db.session.commit()
+        return redirect("/", code=302)
+
+    return render_template("form.html")
+
+
+@app.route("/api/pals")
+def pals():
+    results = db.session.query(Pet.name, Pet.lat, Pet.lon).all()
+
+    hover_text = [result[0] for result in results]
+    lat = [result[1] for result in results]
+    lon = [result[2] for result in results]
+
+    pet_data = [{
+        "type": "scattergeo",
+        "locationmode": "USA-states",
+        "lat": lat,
+        "lon": lon,
+        "text": hover_text,
+        "hoverinfo": "text",
+        "marker": {
+            "size": 50,
+            "line": {
+                "color": "rgb(8,8,8)",
+                "width": 1
+            },
+        }
+    }]
+
+    return jsonify(pet_data)
+
+#runs on the main promt only
+if __name__ == "__main__":
+    app.run()
